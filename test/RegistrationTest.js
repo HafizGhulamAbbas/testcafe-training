8f3d4553d79f1456988584b8fbfe98457f63b00c
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-12-14 14:18:53 +0500

    Data driven testing

--- test/RegistrationTest.js
+++ test/RegistrationTest.js
@@ -4,6 +4,8 @@ import registerPage from "../pages/RegisterPage";
 import loginPage from "../pages/LoginPage";
 import customerPage from "../pages/CustomerPage";
 
+const dataSet = require('../data/data.json');
+
 const URL = "https://demo.nopcommerce.com/";
 const getURL = ClientFunction(() => window.location.href);
 var randomNumber = Math.floor(Math.random() * 10000);
@@ -19,35 +21,37 @@ fixture("Registration Page")
             .expect(homePage.subTitleHeader.exists).ok();
     });
 
-    test('User Registration and Login Test', async t => {
-        await t
-            .click(homePage.registerLink)
-            .expect(getURL()).contains('register')
-            .click(registerPage.genderOption)
-            .typeText(registerPage.firstName, 'Ghulam')
-            .typeText(registerPage.lastName, 'Abbas');
-        await registerPage.selectDay('2');
-        await registerPage.selectMonth('May');
-        await registerPage.selectYear('1992');
-        await t
-            .typeText(registerPage.email, userEmail)
-            .typeText(registerPage.password, '123456')
-            .typeText(registerPage.confirmPassword, '123456')
-            .click(registerPage.registerButton)
-            .expect(registerPage.successMessage.exists).ok()
-        // Logout
-            .click(homePage.logoutLink)
-        // Login with Registered Account
-            .click(homePage.loginLink)
-            .expect(loginPage.accountHeader.exists).ok()
-            .typeText(loginPage.emailInput, userEmail)
-            .typeText(loginPage.passwordInput, '123456')
-            .click(loginPage.submitButton)
-        // Goto My Account
-            .click(homePage.myAccountLink)
-        // Check Orders is displayed
-            .expect(customerPage.ordersLink.exists).ok()
-            .click(customerPage.ordersLink)
-            .expect(customerPage.noOrdersLabel.exists).ok()
-            .takeScreenshot();
+    dataSet.forEach(data => {
+        test('User Registration and Login Test', async t => {
+            await t
+                .click(homePage.registerLink)
+                .expect(getURL()).contains('register')
+                .click(registerPage.genderOption)
+                .typeText(registerPage.firstName, data.firstName)
+                .typeText(registerPage.lastName, data.lastName);
+            await registerPage.selectDay(data.birthDay);
+            await registerPage.selectMonth(data.birthMonth);
+            await registerPage.selectYear(data.birthYear);
+            await t
+                .typeText(registerPage.email, data.email + randomNumber + '@test.com')
+                .typeText(registerPage.password, data.password)
+                .typeText(registerPage.confirmPassword, data.password)
+                .click(registerPage.registerButton)
+                .expect(registerPage.successMessage.exists).ok()
+            // Logout
+                .click(homePage.logoutLink)
+            // Login with Registered Account
+                .click(homePage.loginLink)
+                .expect(loginPage.accountHeader.exists).ok()
+                .typeText(loginPage.emailInput, data.email + randomNumber + '@test.com')
+                .typeText(loginPage.passwordInput, data.password)
+                .click(loginPage.submitButton)
+            // Goto My Account
+                .click(homePage.myAccountLink)
+            // Check Orders is displayed
+                .expect(customerPage.ordersLink.exists).ok()
+                .click(customerPage.ordersLink)
+                .expect(customerPage.noOrdersLabel.exists).ok()
+                .takeScreenshot();
+        });
     });
